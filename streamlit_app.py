143cd34ce7e701ad81849a0e4a06e436bc6665cf
Ricardo Vicencio <159567237+RicVic95@users.noreply.github.com>
2025-03-14 18:21:19 +0000

    this will probably work now

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write("Choose the fruits you want in your custom Smoothie")
 # Insert widget 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('SEARCH_ON'))
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('SEARCH_ON'), col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
