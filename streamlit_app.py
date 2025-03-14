38488e8cdd3e2b29efdd018450ced0bb6f288b3f
Ricardo Vicencio <159567237+RicVic95@users.noreply.github.com>
2025-03-14 18:10:52 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.stop()
 
 # Convert snowpark df to pandas df
 pd_df = my_dataframe.to_pandas()
