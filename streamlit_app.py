f188068551564962a5932ba3a4444cd172acebd1
Ricardo Vicencio <159567237+RicVic95@users.noreply.github.com>
2025-03-14 18:11:31 +0000

    dienxdx

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write("Choose the fruits you want in your custom Smoothie")
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 # Convert snowpark df to pandas df
