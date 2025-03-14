6cb4d567fd5685a1fe0aadc57c59b8b70048a9e1
Ricardo Vicencio <159567237+RicVic95@users.noreply.github.com>
2025-03-14 18:13:06 +0000

    message

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.write("Choose the fruits you want in your custom Smoothie")
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 # Convert snowpark df to pandas df
 pd_df = my_dataframe.to_pandas()
@@ -27,7 +27,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 # Ingredients           # 
 # --------------------- # 
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
+ingredients_list = st.multiselect('Choose up to 5 ingredients', pd_df, max_selections=5)
 if ingredients_list: 
     # Create empty string to store values
     ingredients_string = ''
